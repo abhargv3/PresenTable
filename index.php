1c4563b97b375e3746cdf66e56fe5ef0d4010e1a
Aditya Bhargava <abhargv3@illinois.edu>
2016-11-19 14:29:21 -0600

    some more changes

--- index.php
+++ index.php
@@ -11,17 +11,17 @@
       <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
       </link>
 </head>
-  <body ng-app="myApp">
-    <div class="container-fluid" ng-controller="homeCtrl">
-    {{11+12}}
+  <body ng-app>
+    <div class="container-fluid">
+    {{11+2}}
       <border1>
           <h1>
            <strong>Welcome to PresenTable </strong>
          </h1>
       </border1>
       <p class = "border1">PresenTable fixes up your Facebook page to make you more presentable to recruiters </p> 
-      <button type="button" class="btn btn-info btn-lg" ng-click=FBLogin()>Login</button>
-      <a class="btn btn-info btn-lg active" role="button" aria-pressed="true" ng-click=FBImage()>Be PresenTable </a>
+      <button type="button" class="btn btn-info btn-lg">Login</button>
+      <a class="btn btn-info btn-lg active" role="button" aria-pressed="true">Be PresenTable </a>
 
   </div>
 
