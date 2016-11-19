3a0d92b4bcbc633da38d3ebd82900d16226c4322
Vinay Shanbhag <Vinay Shanbhag>
2016-11-19 10:19:05 -0600

    i am vingay1

--- index.php
+++ index.php
@@ -1,1 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Classes -->
 
+p.border1{
+	border: 2px solid black;
+	border-radius: 5px;
+}
+
+
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+<div class="container-fluid">
+
+<border1><h1><strong>PresenTable</strong></h1></border1>
+
+
+
+
+<!------------------------------------------------------>
+
+<div class="row">
+  <div class="col-xs-12 col-md-8">.col-xs-12 .col-md-8</div>
+  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+</div>
+
+<!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+<div class="row">
+  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+</div>
+
+<!-- Columns are always 50% wide, on mobile and desktop -->
+<div class="row">
+  <div class="col-xs-6">.col-xs-6</div>
+  <div class="col-xs-6">.col-xs-6</div>
+</div>
+
+<!------------------------------------------------------>
+
+
+
+  <p>This is some text.</p> 
+	<p>This is a rounded image </p>
+	
+</div>
+
+</body>
+</html>
