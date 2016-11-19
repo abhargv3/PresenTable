bc1ef4c49a1f5180e7ed2d243a409707109a65cc
Vinay Shanbhag <Vinay Shanbhag>
2016-11-19 11:48:07 -0600

    Trying the bootstrap CDN-vinay5

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <!-- Classes -->
 
 <head>
-    <title> PresenTable-Login </title>
+    <title> PresenTable</title>
       <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 
@@ -18,7 +18,7 @@
 
 <!--==============================================-->
 <!--===========================================-->
-     <p>This is some text.</p> 
+     <p class = border1">This is some text.</p> 
 
 	
 
