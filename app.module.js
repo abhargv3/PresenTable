4c957ddaffd4267924f05bf31612467aa720f617
Aditya Bhargava <abhargv3@illinois.edu>
2016-11-19 14:13:25 -0600

    debugging

--- app.module.js
+++ app.module.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", ['ngRoute']);
+var app=angular.module("myApp",["ngRoute"]);
 
 window.fbAsyncInit = function() {
     FB.init({
--- index.php
+++ index.php
@@ -7,7 +7,6 @@
       <!-- Latest compiled and minified AngularJavaScript -->
       <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
       <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular-route.js"></script>
-"
       <!-- Latest compiled and minified Bootstrap CSS -->
       <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
       </link>
