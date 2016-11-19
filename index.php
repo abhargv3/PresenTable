fdb4a0ffb7e2ea80c00163aabb5e17aa0fc594f3
Aditya Bhargava <abhargv3@illinois.edu>
2016-11-19 12:52:07 -0600

    added facebook func

--- index.php
+++ index.php
@@ -5,26 +5,20 @@
 <head>
     <title> PresenTable</title>
       <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <script type="text/javascript" src="app.module.js"></script>
 </head>
 
-  <body>
-    <div class="container-fluid">
-
-    <border1>
-        <h1>
-         <strong>Welcome to PresenTable </strong>
-       </h1>
-    </border1>
-
-<!--==============================================-->
-<!--===========================================-->
-     <p class = "border1">PresenTable fixes up your Facebook page to make you more presentable to recruiters </p> 
-
-   <button type="button" class="btn btn-info btn-lg">Login</button>
-	
-   <a href="#" class="btn btn-info btn-lg active" role="button" aria-pressed="true">Be PresenTable </a>
-
-
+  <body ng-app="myApp">
+    <div class="container-fluid" ng-controller="homeCtrl">
+    {{2+3}}
+      <border1>
+          <h1>
+           <strong>Welcome to PresenTable </strong>
+         </h1>
+      </border1>
+      <p class = "border1">PresenTable fixes up your Facebook page to make you more presentable to recruiters </p> 
+      <button type="button" class="btn btn-info btn-lg" ng-click=FBImage()>Login</button>
+      <a href="#" class="btn btn-info btn-lg active" role="button" aria-pressed="true">Be PresenTable </a>
 
   </div>
 
