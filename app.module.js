a82fbb2c377a4bd9ae8123bfd12bdb807cb47941
Aditya Bhargava <abhargv3@illinois.edu>
2016-11-19 12:45:36 -0600

    added module js files

---
+++ app.module.js
@@ -0,0 +1,32 @@
+var app = angular.module("myApp", []);
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '152005755273941',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+app.controller("homeCtrl", function($scope){
+
+	$scope.FBImage=function(){
+        FB.api(
+  			'/me/photos',
+  			'GET',
+  			{},
+  			function(response) {
+      // Insert your code here
+      			console.log(response);
+  	}
+	);
+        
+};
