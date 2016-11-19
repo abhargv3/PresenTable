1d10f0655db9c965ff5c364ad56f7a1e72ecc3e0
Vinay Shanbhag <Vinay Shanbhag>
2016-11-19 11:43:47 -0600

    Trying the bootstrap CDN-vinay3

--- index.php
+++ index.php
@@ -2,44 +2,29 @@
 
 <!-- Classes -->
 
-
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="css/index.css">
+<head>
+    <title> PresenTable-Login </title>
+      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
-<body>
-	<h3></h3> -->
 
+  <body>
+    <div class="container-fluid">
 
-
-
-<div class="container-fluid">
-
-<border1><h1><strong>Welcome to PresenTable </strong></h1></border1>
+    <border1>
+        <h1>
+         <strong>Welcome to PresenTable </strong>
+       </h1>
+    </border1>
 
 <!--==============================================-->
-
-<div class="row">
-  <div class="col-xs-12 col-md-8">.col-xs-12 .col-md-8</div>
-  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-</div>
-
-
 <!--===========================================-->
+     <p>This is some text.</p> 
 
-
-
-  <p>This is some text.</p> 
-	<p>This is a rounded image </p>
 	
-</div>
 
-</body>
+
+
+  </div>
+
+  </body>
 </html>
